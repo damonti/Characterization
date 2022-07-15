68411200aa696116cebc4d76d1703fa700edbef1
Dario <da.monticelli@gmail.com>
2022-07-15 15:10:54 +0200

    fixed bugs in scripts

--- sim/mux/scripts/xcelium.d/run.lnx86.19.03.d/files.ts
+++
@@ -1,3 +0,0 @@
-1534985301 /home_old/tech/tsmc/40nm/TSMCHOME/digital/Front_End/verilog/tcbn40lpbwp_120a/tcbn40lpbwp.v
-1657186148 /home/20200969/Estimation/rtl/mux/netlist/mux_2-1_8BW_netlist.v
-1657876756 /home/20200969/Estimation/rtl/mux/test_mux_temp.v
