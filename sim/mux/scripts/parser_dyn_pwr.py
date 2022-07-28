5f3473b4ca383a93c5ffb0d7c6080baabb57af88
Dario <da.monticelli@gmail.com>
2022-07-28 10:02:41 +0200

    updated /sim/mux directory

--- sim/mux/scripts/parser_dyn_pwr.py
+++ sim/mux/scripts/parser_dyn_pwr.py
@@ -9,7 +9,7 @@ def pwr_to_list(CONFIGS):
     targets = []
     for CONFIG in range (0, len(CONFIGS)):
         #file_path = RPT_PATH + "/mux.mux_" + str(CONFIGS[CONFIG][0]) + "-1_" + str(CONFIGS[CONFIG][1]) + "BW_" + str(CONFIGS[CONFIG][2]) + "percent_payload.rpt"
-        file_path = RPT_PATH + "/mux.mux_2-1_" + str(CONFIGS[CONFIG][0]) + "BW_" + str(CONFIGS[CONFIG][1]) + "percent_payload.rpt"
+        file_path = RPT_PATH + "/mux.mux_"+ str(CONFIGS[CONFIG][0])+ "-1_" + str(CONFIGS[CONFIG][1]) + "BW_" + str(CONFIGS[CONFIG][2]) + "percent_payload.rpt"
         with open(file_path, 'r', encoding='utf-8') as f:
             data = f.readlines()
             targets.append(data[17].strip().split()[4])
@@ -22,22 +22,22 @@ def config_generation(IN):
     MAX_BITWIDTH = 64
     SUB_LIST = []
     LIST = []
-    #IN = 2
-    for BW in range(8, MAX_BITWIDTH+1): #SWEEP THROUGH BITWIDTHS
-        min_step = 100/BW  # minimum percentage step
-        for i in range(1, BW+1):  # SWEEP THOROUGH PERCENTAGES
-            #SUB_LIST = [IN, BW, round(min_step*i)]
-            SUB_LIST = [BW, round(min_step*i)]
-            LIST.append(SUB_LIST)
+    for input in range(2, IN+1):
+        for BW in range(8, MAX_BITWIDTH+1): #SWEEP THROUGH BITWIDTHS
+            min_step = 100/BW  # minimum percentage step
+            for i in range(1, BW+1):  # SWEEP THOROUGH PERCENTAGES
+                #SUB_LIST = [IN, BW, round(min_step*i)]
+                SUB_LIST = [input, BW, round(min_step*i)]
+                LIST.append(SUB_LIST)
     return LIST
 
-IN=2
+IN=4
 CONFIGS = config_generation(IN) #generate the mux configurations
 TARGETS = pwr_to_list(CONFIGS) #generate a csv out of the power data. the CONFIGS is needed to find the parametrized report name
 
 #DATA FRAME
-#header = ['INPUT', 'BW', 'PERCENTAGE']
-header = ['BW', 'PERCENTAGE']
+header = ['INPUT', 'BW', 'PERCENTAGE']
+#header = ['BW', 'PERCENTAGE']
 df = pd.DataFrame(CONFIGS, columns=header)
 df['Energy [fJ]'] = TARGETS
 #print (df)
