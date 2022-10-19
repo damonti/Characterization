a2c709ea16cc4f8f56ba337f6ca5e8eee9ec6038
damonti <da.monticelli@gmail.com>
2022-10-19 09:54:26 +0200

    add adder_fir & multiplier_fir

---
+++ sim/fir/multiplier_fir/scripts/dump_models.py
@@ -0,0 +1,119 @@
+#Importing required packages.
+from locale import dcgettext
+import pandas as pd
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+plt.rc('text', usetex=True) 
+from matplotlib import cm  
+#from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, accuracy_score, mean_absolute_percentage_error
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn import ensemble, linear_model
+import os
+import numpy as np
+from numpy import mean
+from numpy import absolute
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+import xgboost as xgb
+import scikitplot as skplt
+from sklearn.svm import SVR
+from scipy import stats
+from sklearn.tree import DecisionTreeRegressor
+#from parameter_tuning.py derive the best parameters for each model given the dataset
+#then use this to dump the models with the best fitting parameters
+import pickle
+import time
+
+DESIGN ="multiplier_fir"
+SIM_PATH = os.path.expanduser("~/Estimation/sim/fir/"+DESIGN)
+DF_PATH = SIM_PATH+ "/dataframe/"+DESIGN+"_configurations.csv"
+df = pd.read_csv(DF_PATH, sep=',')
+
+df_energy = df[['BW', 'PERCENTAGE', 'Power [nW]']].copy()
+df_area = df[['BW', 'Area [um^2]']].copy()
+
+
+X_list_energy = df_energy.drop('Power [nW]', axis=1).values #"INPUT", "BW" and "PERCENTAGE"
+X_list_area = df_area.drop('Area [um^2]', axis=1).values #"INPUT" and "BW"  
+
+y_energy = df_energy['Power [nW]'].values 
+y_area = df_area['Area [um^2]'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X_list_energy, y_energy, test_size=0.2, random_state=4, shuffle=True)
+
+
+#EXTREME GRADIENT BOOSTING
+params = {
+    'n_estimators': 1800,
+    "max_depth": 3,
+    "min_samples_split": 3,
+    "learning_rate": 0.1,
+    "loss": 'squared_error',
+}
+
+reg_xgb = ensemble.GradientBoostingRegressor(**params)
+start = time.time()
+reg_xgb.fit(X_train, y_train)
+stop = time.time()
+print(f"Training time XGB: {stop - start}s")
+with open('xgb_model_mux', 'wb') as f:
+    pickle.dump(reg_xgb, f)
+f.close()
+
+#to load it:
+#with open ('xgb_model', 'rb') as f:
+#    xgb_model = pickle.load(f)
+
+#LASSO
+# reg_lasso = linear_model.Lasso(alpha=0.1)
+# start = time.time()
+# reg_lasso.fit(X_train, y_train)
+# stop = time.time()
+# print(f"Training time lasso: {stop - start}s")
+# with open('lasso_model_mux', 'wb') as f:
+#     pickle.dump(reg_lasso, f)
+# f.close()
+
+# #Random forest
+# reg_forest = RandomForestRegressor(max_features=10, n_estimators = 100)
+# start = time.time()
+# reg_forest.fit(X_train, y_train)
+# stop = time.time()
+# print(f"Training time random forest: {stop - start}s")
+# with open('RandomForest_model_mux', 'wb') as f:
+#     pickle.dump(reg_forest, f)
+# f.close()
+
+# #Decision tree
+# reg_dc = DecisionTreeRegressor(max_depth=100)
+# start = time.time()
+# reg_dc.fit(X_train, y_train)
+# stop = time.time()
+# print(f"Training time decision tree: {stop - start}s")
+# with open('DecisionTree_model_mux', 'wb') as f:
+#     pickle.dump(reg_dc, f)
+# f.close()
+
+
+# #SVR
+# reg_svr = SVR(kernel='linear', C=10, epsilon=1)
+# start = time.time()
+# reg_svr.fit(X_train, y_train)
+# stop = time.time()
+# print(f"Training time svr: {stop - start}s")
+# with open('svr_model_mux', 'wb') as f:
+#     pickle.dump(reg_svr, f)
+# f.close()
+
+# #LINEAR REGRESSION
+# reg_lr = LinearRegression()
+# start = time.time()
+# reg_lr.fit(X_train, y_train)
+# stop = time.time()
+# print(f"Training time linear reg: {stop - start}s")
+# with open('linear_model_mux', 'wb') as f:
+#     pickle.dump(reg_lr, f)
+# f.close()
---
+++ sim/fir/multiplier_fir/scripts/table_gen.py
@@ -0,0 +1,71 @@
+#This parses the reports from jules and creates a dataframe with the features 
+
+import os
+import pandas as pd
+
+def area_to_list(CONFIGS, SIM_PATH):
+    """Returns a list of the area numbers"""
+    RPT_PATH = SIM_PATH+"/reports"
+    targets = []
+    for CONFIG in range (0, len(CONFIGS)):
+        file_path = RPT_PATH + "/" + DESIGN + "_" + str(CONFIGS[CONFIG][0]) + "BW_netlist.area.rpt" #pick the corresponding area for the corresponding configuration
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = f.readlines()
+            targets.append(data[13].strip().split()[12]) #append all the power numbers from the reports in a list
+    targets_float = [format(float(i), ".3f") for i in targets] #i is in nW, T_clk is 10ns, to retrieve energy in fJ, simply divide by 100.
+    return targets_float
+
+def pwr_to_list(CONFIGS, SIM_PATH):
+    """Returns a list of the dynamic power numbers"""
+    RPT_PATH = SIM_PATH+"/reports"
+    targets = []
+    for CONFIG in range (0, len(CONFIGS)):
+        file_path = RPT_PATH + "/" + DESIGN + "_" + str(CONFIGS[CONFIG][0]) + "BW_" + str(CONFIGS[CONFIG][1]) + "percent_avg.rpt"
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = f.readlines()
+            targets.append(data[17].strip().split()[4]) #append all the power numbers from the reports in a list
+    targets_float = [format(float(i), ".4f") for i in targets] #i is in nW, T_clk is 10ns, to retrieve energy in fJ, simply divide by 100.
+    return targets_float
+
+
+def config_generation():
+    """It returns a list of lists with the 2 parameters of the desjgn sweeping in order"""
+    MAX_BITWIDTH = 30
+    SUB_LIST = []
+    LIST = []
+    for BW in range(2, MAX_BITWIDTH+1, 2): #SWEEP THROUGH BITWIDTHS
+        min_step = 100/BW  # minimum percentage step
+        for i in range(1, BW+1):  # SWEEP THOROUGH PERCENTAGES
+            SUB_LIST = [BW, round(min_step*i)] #SUBLIST = [BW, PERCENTAGE]
+            LIST.append(SUB_LIST)
+    return LIST
+
+DESIGN ="multiplier_fir"
+SIM_PATH = os.path.expanduser("~/Estimation/sim/fir/"+DESIGN)
+CONFIGS = config_generation() #generate the adder configurations
+POWER_TARGETS = pwr_to_list(CONFIGS, SIM_PATH) #generate a csv out of the power data. the CONFIGS is needed to find the parametrized report name
+AREA_TARGETS = area_to_list(CONFIGS, SIM_PATH)
+#DATA FRAME
+header = ['technology [nm]', 'latency [ns]', 'datawidth', 'num_pipeline_stages', 'toggling activity [%]', 'action', 'energy [fJ]', 'area [um^2]']
+#df = pd.DataFrame(CONFIGS, columns=header)
+df = pd.DataFrame(columns=header)
+
+df['datawidth'] = [item[0] for item in CONFIGS]
+df['technology [nm]'] = '40'
+df['latency [ns]'] = '10'
+df['num_pipeline_stages'] = '0'
+df['toggling activity [%]'] = [item[1] for item in CONFIGS]
+df['action'] = 'add'
+df['power [nW]'] = POWER_TARGETS
+df['area [um^2]'] = AREA_TARGETS
+
+#print (df)
+PATH = os.path.expanduser("~/Estimation/tables/")
+df.to_csv(PATH + DESIGN + '.csv', index=False)
+
+
+
+
+
+
+
